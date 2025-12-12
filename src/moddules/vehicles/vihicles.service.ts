854db8feb89b638a2af39b42e0ec74269b3e7d3e
trmtanjil <trmtanjil02@gmail.com>
2025-12-12 14:02:33 +0600

    vehicles service get singlesget put delet operaton  succesfully

--- src/moddules/vehicles/vihicles.service.ts
+++ src/moddules/vehicles/vihicles.service.ts
@@ -19,8 +19,41 @@ const creatVihicles = async (payload:Record<string,unknown>)=>{
     return result;
 }
 
+const getVehicles = async()=>{
+    const result= await pool.query(`SELECT * FROM vahicles`);
+    return result;
+
+}
+
+const singleVehicles = async(id:string)=>{
+    const result = await pool.query(`SELECT * FROM vehicles WHERE id = $1`,[id])
+    return result;
+}
+const updateVehicles = async(paylod:Record<string,unknown>)=>{
+    const {vehicle_name,type,registration_number,availability_status,updated_at,id} = paylod;
+    const result = await pool.query(`UPDATE vehicles SET  vehicle_name = $1,
+        type = $2,
+        registration_number = $3,
+        daily_rent_price = $4,
+        availability_status = $5,
+        updated_at = NOW()
+      WHERE id = $6
+      RETURNING *;`,[vehicle_name,type,registration_number, availability_status,  updated_at, id])
+      return result;
+}
+
+
+const deletvahicles= async(id:string)=>{
+    const result = await pool.query(`DELETE FROM vehicles WHERE id=$1`,[id])
+    return result
+}
 
 export const vahiclesServices={
   creatVihicles,
-  
+  getVehicles,
+  singleVehicles,
+  updateVehicles,
+  deletvahicles,
+
+
 }
